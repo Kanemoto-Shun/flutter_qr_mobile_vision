dc1eba01874c817cd115061c4141c2f455822180
金本 隼 <52392612+Kanemoto-Shun@users.noreply.github.com>
2021-09-06 13:52:05 +0900

    test

--- android/src/main/java/com/github/rmtmckenzie/qrmobilevision/QrCameraC2.java
+++ android/src/main/java/com/github/rmtmckenzie/qrmobilevision/QrCameraC2.java
@@ -201,7 +201,6 @@ class QrCameraC2 implements QrCamera {
                 @Override
                 public void onOpened(@NonNull CameraDevice device) {
                     cameraDevice = device;
-                    manager.setTorchMode(cameraId, true);
                     startCamera();
                 }
 
